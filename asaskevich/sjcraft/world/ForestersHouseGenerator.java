0bb1d236e4a37f0834f54e6a65c7cfb0696d3605
Alex Saskevich <alexhomel@ya.ru>
2014-02-04 14:26:21 +0300

    Removed unused functions, added comments

--- asaskevich/sjcraft/world/ForestersHouseGenerator.java
+++ asaskevich/sjcraft/world/ForestersHouseGenerator.java
@@ -10,7 +10,6 @@ import net.minecraft.util.WeightedRandomChestContent;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.common.ChestGenHooks;
-import net.minecraftforge.common.DungeonHooks;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class ForestersHouseGenerator implements IWorldGenerator {
@@ -25,98 +24,98 @@ public class ForestersHouseGenerator implements IWorldGenerator {
 
 	public void generateSurface(World world, Random random, int chunkX,
 			int chunkZ) {
-		if (random.nextInt(20) != 0) {
+		// Chance of generating structure is 1 : 40 per chunk
+		if (random.nextInt(40) != 0) {
 			return;
 		}
-		for (int l = 0; l < 1; ++l) {
-			int posX = chunkX + random.nextInt(16);
-			int posZ = chunkZ + random.nextInt(16);
-			int posY = random.nextInt(100) + 1;
-			while (world.getBlockId(posX, posY, posZ) != 0) {
-				posY++;
-			}
-			if (world.getBlockId(posX, posY - 1, posZ) != Block.grass.blockID) {
-				return;
-			}
-			String name = world.getBiomeGenForCoords(posX, posZ).biomeName;
-
-			if (name != "Forest") {
-				return;
-			}
-
-			for (int x = posX - 2; x <= posX + 2; x++) {
-				for (int z = posZ - 2; z <= posZ + 2; z++) {
-					for (int y = posY - 5; y <= posY; y++) {
-						world.setBlock(x, y, z, Block.cobblestone.blockID);
-					}
+		// Random position for generating structure
+		int posX = chunkX + random.nextInt(16);
+		int posZ = chunkZ + random.nextInt(16);
+		int posY = random.nextInt(100) + 1;
+		while (world.getBlockId(posX, posY, posZ) != 0) {
+			posY++;
+		}
+		// We can generate it only at blocks of grass
+		if (world.getBlockId(posX, posY - 1, posZ) != Block.grass.blockID) {
+			return;
+		}
+		// And only in biome Forest
+		String name = world.getBiomeGenForCoords(posX, posZ).biomeName;
+		if (name != "Forest") {
+			return;
+		}
+		// Building bottom of house by blocks of cobblestone
+		for (int x = posX - 2; x <= posX + 2; x++) {
+			for (int z = posZ - 2; z <= posZ + 2; z++) {
+				for (int y = posY - 5; y <= posY; y++) {
+					world.setBlock(x, y, z, Block.cobblestone.blockID);
 				}
 			}
-			for (int x = posX - 2; x <= posX + 2; x++) {
-				for (int z = posZ - 2; z <= posZ + 2; z++) {
-					for (int y = posY + 1; y <= posY + 4; y++) {
-						world.setBlock(x, y, z, Block.planks.blockID);
-					}
+		}
+		// Building top of house by blocks of planks
+		for (int x = posX - 2; x <= posX + 2; x++) {
+			for (int z = posZ - 2; z <= posZ + 2; z++) {
+				for (int y = posY + 1; y <= posY + 4; y++) {
+					world.setBlock(x, y, z, Block.planks.blockID);
 				}
 			}
-			for (int x = posX - 1; x <= posX + 1; x++) {
-				for (int z = posZ - 1; z <= posZ + 1; z++) {
-					for (int y = posY + 1; y <= posY + 3; y++) {
-						world.setBlockToAir(x, y, z);
-					}
+		}
+		// Set to air blocks inside house
+		for (int x = posX - 1; x <= posX + 1; x++) {
+			for (int z = posZ - 1; z <= posZ + 1; z++) {
+				for (int y = posY + 1; y <= posY + 3; y++) {
+					world.setBlockToAir(x, y, z);
 				}
 			}
-			world.setBlock(posX - 2, posY + 2, posZ, Block.glass.blockID);
-			world.setBlock(posX + 2, posY + 2, posZ, Block.glass.blockID);
-			world.setBlock(posX, posY + 2, posZ + 2, Block.glass.blockID);
-			world.setBlockToAir(posX, posY + 2, posZ - 2);
-			world.setBlockToAir(posX, posY + 1, posZ - 2);
-			world.setBlock(posX, posY + 3, posZ - 1, Block.torchWood.blockID);
-			for (int y = posY + 1; y <= posY + 4; y++) {
-				world.setBlock(posX - 2, y, posZ - 2, Block.wood.blockID);
-				world.setBlock(posX + 2, y, posZ - 2, Block.wood.blockID);
-				world.setBlock(posX + 2, y, posZ + 2, Block.wood.blockID);
-				world.setBlock(posX - 2, y, posZ + 2, Block.wood.blockID);
-			}
-			world.setBlock(posX - 2, posY + 4, posZ - 1, Block.wood.blockID);
-			world.setBlock(posX - 2, posY + 4, posZ, Block.wood.blockID);
-			world.setBlock(posX - 2, posY + 4, posZ + 1, Block.wood.blockID);
-			world.setBlock(posX + 2, posY + 4, posZ + 1, Block.wood.blockID);
-			world.setBlock(posX + 2, posY + 4, posZ - 1, Block.wood.blockID);
-			world.setBlock(posX + 2, posY + 4, posZ, Block.wood.blockID);
-			world.setBlock(posX + 1, posY + 4, posZ + 2, Block.wood.blockID);
-			world.setBlock(posX - 1, posY + 4, posZ + 2, Block.wood.blockID);
-			world.setBlock(posX, posY + 4, posZ + 2, Block.wood.blockID);
-			world.setBlock(posX + 1, posY + 4, posZ - 2, Block.wood.blockID);
-			world.setBlock(posX, posY + 4, posZ - 2, Block.wood.blockID);
-			world.setBlock(posX - 1, posY + 4, posZ - 2, Block.wood.blockID);
-			// door
-			ItemDoor.placeDoorBlock(world, posX, posY + 1, posZ - 2, 1,
-					Block.doorWood);
+		}
+		// Windows
+		world.setBlock(posX - 2, posY + 2, posZ, Block.glass.blockID);
+		world.setBlock(posX + 2, posY + 2, posZ, Block.glass.blockID);
+		world.setBlock(posX, posY + 2, posZ + 2, Block.glass.blockID);
+		world.setBlockToAir(posX, posY + 2, posZ - 2);
+		world.setBlockToAir(posX, posY + 1, posZ - 2);
+		world.setBlock(posX, posY + 3, posZ - 1, Block.torchWood.blockID);
+		// Pillars
+		for (int y = posY + 1; y <= posY + 4; y++) {
+			world.setBlock(posX - 2, y, posZ - 2, Block.wood.blockID);
+			world.setBlock(posX + 2, y, posZ - 2, Block.wood.blockID);
+			world.setBlock(posX + 2, y, posZ + 2, Block.wood.blockID);
+			world.setBlock(posX - 2, y, posZ + 2, Block.wood.blockID);
+		}
+		// Roof of house
+		world.setBlock(posX - 2, posY + 4, posZ - 1, Block.wood.blockID);
+		world.setBlock(posX - 2, posY + 4, posZ, Block.wood.blockID);
+		world.setBlock(posX - 2, posY + 4, posZ + 1, Block.wood.blockID);
+		world.setBlock(posX + 2, posY + 4, posZ + 1, Block.wood.blockID);
+		world.setBlock(posX + 2, posY + 4, posZ - 1, Block.wood.blockID);
+		world.setBlock(posX + 2, posY + 4, posZ, Block.wood.blockID);
+		world.setBlock(posX + 1, posY + 4, posZ + 2, Block.wood.blockID);
+		world.setBlock(posX - 1, posY + 4, posZ + 2, Block.wood.blockID);
+		world.setBlock(posX, posY + 4, posZ + 2, Block.wood.blockID);
+		world.setBlock(posX + 1, posY + 4, posZ - 2, Block.wood.blockID);
+		world.setBlock(posX, posY + 4, posZ - 2, Block.wood.blockID);
+		world.setBlock(posX - 1, posY + 4, posZ - 2, Block.wood.blockID);
+		// Door
+		ItemDoor.placeDoorBlock(world, posX, posY + 1, posZ - 2, 1,
+				Block.doorWood);
+		// Spawn forester inside house
+		EntityVillager entityvillager = new EntityVillager(world, 0);
+		entityvillager.setLocationAndAngles((double) posX + 0.5D,
+				(double) posY + 1, (double) posZ + 0.5D, 0.0F, 0.0F);
+		world.spawnEntityInWorld(entityvillager);
+		// Chest in house
+		world.setBlock(posX - 1, posY + 1, posZ - 1, Block.chest.blockID, 0, 2);
+		TileEntityChest tileentitychest = (TileEntityChest) world
+				.getBlockTileEntity(posX - 1, posY + 1, posZ - 1);
 
-			EntityVillager entityvillager = new EntityVillager(world, 0);
-			entityvillager.setLocationAndAngles((double) posX + 0.5D,
-					(double) posY + 1, (double) posZ + 0.5D, 0.0F, 0.0F);
-			world.spawnEntityInWorld(entityvillager);
-			// chest
-			world.setBlock(posX - 1, posY + 1, posZ - 1, Block.chest.blockID,
-					0, 2);
-			TileEntityChest tileentitychest = (TileEntityChest) world
-					.getBlockTileEntity(posX - 1, posY + 1, posZ - 1);
-
-			if (tileentitychest != null) {
-				ChestGenHooks info = ChestGenHooks
-						.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH);
-				WeightedRandomChestContent.generateChestContents(random,
-						info.getItems(random), tileentitychest,
-						info.getCount(random));
-			}
-
+		if (tileentitychest != null) {
+			ChestGenHooks info = ChestGenHooks
+					.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH);
+			WeightedRandomChestContent.generateChestContents(random,
+					info.getItems(random), tileentitychest,
+					info.getCount(random));
 		}
 
-	}
-
-	private String pickMobSpawner(Random par1Random) {
-		return DungeonHooks.getRandomDungeonMob(par1Random);
 	}
 
 }
